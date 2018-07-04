2bfb2d37c0b3f509960449d26097c855930f3a7f
Ney Moura <neymourac@unifor.br>
2018-07-04 11:53:44 -0300

    Internacionalização para pr-BR

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module ApiAgendaTelefonica
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.encoding = "utf-8"
+    config.i18n.default_locale = :'pt-BR'
+
   end
 end
